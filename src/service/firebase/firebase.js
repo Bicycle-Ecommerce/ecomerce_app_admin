d41b4e489da7ad035854dfbb85fdbca48f7702e1
Minh Triet Nguyen <90248665+MinhTriet0612@users.noreply.github.com>
2024-05-03 10:05:58 +0700

    Update firebase.js

--- src/service/firebase/firebase.js
+++ src/service/firebase/firebase.js
@@ -5,11 +5,7 @@ import { getStorage } from "firebase/storage";
 import "firebase/storage";
 
 const app = initializeApp({
-  apiKey: "AIzaSyBVMldjskaps7wAg3JAuhlVHjqCvpRjKaE",
-  authDomain: "flower-shop-caa2e.firebaseapp.com",
-  projectId: "flower-shop-caa2e",
-  storageBucket: "flower-shop-caa2e.appspot.com",
-  messagingSenderId: "945577682273",
+
 });
 
 export const db = getFirestore();
